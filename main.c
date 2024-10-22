61906107af8efcbe92c92fc65f26c46cf199f52b
Marko <dmkds321@gmail.com>
2024-10-22 19:00:56 -0300

    Adicionando as funções de desenhar uma tela

---
+++ main.c
@@ -0,0 +1,83 @@
+// Bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <locale.h>
+
+// Função para posicionar o cursor em um determinado ponto da tela
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = (short)x;
+    coord.Y = (short)y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+// Função da tela padrão
+void tela()
+{
+    int lin;
+    int col;
+
+    system("cls");
+    system("color 7C");
+    for (lin = 1; lin < 25; lin++)
+    {
+        gotoxy(01, lin);
+        printf("|");
+
+        gotoxy(79, lin);
+        printf("|");
+    }
+    for (col = 1; col < 80; col++)
+    {
+        gotoxy(col, 01);
+        printf("-");
+
+        gotoxy(col, 04);
+        printf("-");
+
+        gotoxy(col, 22);
+        printf("-");
+
+        gotoxy(col, 24);
+        printf("-");
+    }
+    gotoxy(01, 01);
+    printf("+");
+    gotoxy(79, 01);
+    printf("+");
+
+    gotoxy(01, 04);
+    printf("+");
+    gotoxy(79, 04);
+    printf("+");
+
+    gotoxy(01, 22);
+    printf("+");
+    gotoxy(79, 22);
+    printf("+");
+
+    gotoxy(01, 24);
+    printf("+");
+    gotoxy(79, 24);
+    printf("+");
+
+    gotoxy(02, 02);
+    printf("UNICV");
+
+    gotoxy(61, 02);
+    printf("Estrutura de dados");
+
+    gotoxy(67, 03);
+    printf("Marcos Andre");
+
+    gotoxy(02, 23);
+    printf("MSG: ");
+}
+
+int main()
+{
+    
+}
